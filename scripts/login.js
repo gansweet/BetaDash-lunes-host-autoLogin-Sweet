ee24113c12b0f548feef09da60a4c9621a2eb309
gansweet <solomongan@proton.me>
2025-08-27 20:17:12 +0800

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -43,8 +43,8 @@ async function sendTelegramPhoto(filePath, caption = '') {
         const page = await browser.newPage();
         await page.goto(LOGIN_URL, { waitUntil: 'networkidle2' });
 
-        await page.type('input[name="placeholder"]', USERNAME);
-        await page.type('input[name="placeholder"]', PASSWORD);
+        await page.type('input[name="username"]', USERNAME);
+        await page.type('input[name="password"]', PASSWORD);
         await Promise.all([
             page.click('button[type="submit"]'),
             page.waitForNavigation({ waitUntil: 'networkidle2' })
