70ebab7ca1599d02bd00e0fa3373687cf9ca28c6
gansweet <solomongan@proton.me>
2025-08-28 10:44:06 +0800

    Update keepalive.js

--- keepalive.js
+++ keepalive.js
@@ -21,7 +21,7 @@ async function checkSession() {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
         "Connection": "keep-alive",
-        "Referer": url
+        "Referer": DASHBOARD_URL
       }
     });
 
