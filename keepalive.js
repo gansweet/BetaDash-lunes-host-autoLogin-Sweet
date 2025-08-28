1dea8a2207a2d9a6648df8b6ae1487bdec30feb2
gansweet <solomongan@proton.me>
2025-08-28 11:16:51 +0800

    Update keepalive.js

--- keepalive.js
+++ keepalive.js
@@ -27,7 +27,7 @@ async function sendTelegramMessage(message) {
       "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
       "Accept-Encoding": "gzip, deflate, br, zstd",
       "Accept-Language": "en-US,en;q=0.9",
-      "Referer": "https://betadash.lunes.host/servers/35991",
+      "Referer": "https://ctrl.lunes.host/auth/login",
       "Sec-Fetch-Dest": "document",
       "Sec-Fetch-Mode": "navigate",
       "Sec-Fetch-Site": "none",
