b65bee5f75f992c1361d3bce2eb6494af5c72301
gansweet <solomongan@proton.me>
2025-08-28 11:18:56 +0800

    Update keepalive.js

--- keepalive.js
+++ keepalive.js
@@ -22,12 +22,12 @@ async function sendTelegramMessage(message) {
 (async () => {
   try {
     const headers = {
-      "Cookie": COOKIE,
+      "Cookie": process.env.LUNES_COOKIE,
       "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36",
       "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
       "Accept-Encoding": "gzip, deflate, br, zstd",
       "Accept-Language": "en-US,en;q=0.9",
-      "Referer": "https://ctrl.lunes.host/auth/login",
+      "Referer": "https://betadash.lunes.host",
       "Sec-Fetch-Dest": "document",
       "Sec-Fetch-Mode": "navigate",
       "Sec-Fetch-Site": "none",
